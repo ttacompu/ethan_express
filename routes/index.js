9c67070b65056b4bad18de8b0473013e317c4b09
ttacompu <ttacompu@gmail.com>
2016-10-16 17:35:35 -0400

    added fortune module

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,8 @@
 var express = require('express');
-var router = express.Router();
 
-var fortunes = [
-"Conquer your fears or they will conquer you.",
-"Rivers need springs.",
-"Do not fear what you don't know.",
-"You will have a pleasant surprise.",
-"Whenever possible, keep it simple.",
-];
+var fortune = require("../library/fortune");
+
+var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
@@ -15,8 +10,7 @@ router.get('/', function (req, res, next) {
 });
 
 router.get('/about', function (req, res, next) {
- var ranFortune= fortunes[Math.floor(Math.random() * fortunes.length)]
-  res.render('about', {fortune : ranFortune});
+  res.render('about', {fortune : fortune.getFortune()});
 });
 
 
