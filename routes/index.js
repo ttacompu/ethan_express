ff465e868b21bd0803f176686d5af102a886d651
ttacompu <ttacompu@gmail.com>
2016-11-12 18:28:59 -0500

    jquery\

--- routes/index.js
+++ routes/index.js
@@ -6,22 +6,27 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('home');
+    res.render('home');
 });
 
 router.get('/about', function (req, res, next) {
-  res.render('about', 
-  {
-    fortune : fortune.getFortune(),
-    pageTestScript: '/qa/tests-about.js'
-  });
+    res.render('about',
+        {
+            fortune: fortune.getFortune(),
+            pageTestScript: '/qa/tests-about.js'
+        });
 });
 
 router.get('/newsletter', function (req, res, next) {
     res.render('newsletter', { csrf: 'CSRF token goes here' });
 });
 
+router.get('/newsletter/archive', function (req, res, next) {
+    res.render('archive');
+});
+
 router.get('/thankyou', function (req, res, next) {
+    
 
     res.render('thankyou');
 })
@@ -35,6 +40,12 @@ router.post('/process', function (req, res, next) {
         console.log('CSRF token (from hidden form field): ' + req.body._csrf);
         console.log('Name (from visible form field): ' + req.body.name);
         console.log('Email (from visible form field): ' + req.body.email);
+
+        req.session.flash = {
+        type: 'success',
+        intro: 'Thank you!',
+        message: 'You have now been signed up for the newsletter.'};
+
         res.redirect(303, '/thankyou');
     }
 })
